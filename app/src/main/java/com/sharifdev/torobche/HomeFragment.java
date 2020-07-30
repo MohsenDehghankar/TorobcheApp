86efc42cca2285e44e6199e0baddde97b1459f54
z <z.y.j.1379@gmail.com>
2020-07-30 21:30:46 +0430

    add question_layout.xml

--- app/src/main/java/com/sharifdev/torobche/HomeFragment.java
+++ app/src/main/java/com/sharifdev/torobche/HomeFragment.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.parse.ParseUser;
+
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
