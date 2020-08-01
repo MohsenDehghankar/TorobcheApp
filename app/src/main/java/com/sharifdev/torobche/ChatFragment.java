e9826a70177a2cd6dafdb2760ac365a6cdc883ae
Lashgari <Lashgari@DESKTOP-UU1FHJO>
2020-08-01 15:47:11 +0430

    add chat adapter

--- app/src/main/java/com/sharifdev/torobche/ChatFragment.java
+++ app/src/main/java/com/sharifdev/torobche/ChatFragment.java
@@ -4,12 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class ChatFragment extends Fragment {
+    private ArrayList<String> names = new ArrayList<>();
+    private ArrayList<Integer> imageIds = new ArrayList<>();
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_chat, container, false);
+
+        names.add("zahra");
+        imageIds.add(R.drawable.plus);
+
+        ListView userList = rootView.findViewById(R.id.user_list);
+        ChatAdapter chatAdapter = new ChatAdapter(getContext(), imageIds, names);
+        userList.setAdapter(chatAdapter);
+        return rootView;
     }
 }
