59a78303e89337e922d163b7128614c89de5a1b4
Lashgari <Lashgari@DESKTOP-UU1FHJO>
2020-08-01 16:59:07 +0430

    add chat page

--- app/src/main/java/com/sharifdev/torobche/ChatFragment.java
+++ app/src/main/java/com/sharifdev/torobche/ChatFragment.java
@@ -8,6 +8,8 @@ import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
 import java.util.ArrayList;
 
 public class ChatFragment extends Fragment {
@@ -17,12 +19,20 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView =  inflater.inflate(R.layout.fragment_chat, container, false);
 
+        //todo
         names.add("zahra");
+        imageIds.add(R.drawable.plus);
+
+        names.add("zizi");
         imageIds.add(R.drawable.plus);
 
         ListView userList = rootView.findViewById(R.id.user_list);
         ChatAdapter chatAdapter = new ChatAdapter(getContext(), imageIds, names);
         userList.setAdapter(chatAdapter);
+
+
+        MaterialSearchBar searchBar = rootView.findViewById(R.id.searchBar);
+
         return rootView;
     }
 }
