7545d8ad605addddf01c13b85c48c2b0fd847064
MohsenDehghankar <mohsendehghankar@gmail.com>
2020-08-03 01:45:43 +0430

    add category icons and show

--- app/src/main/java/com/sharifdev/torobche/Category/SelectCategoryActivity.java
+++ app/src/main/java/com/sharifdev/torobche/Category/SelectCategoryActivity.java
@@ -10,11 +10,13 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sharifdev.torobche.R;
+import com.sharifdev.torobche.backUtils.CategoryUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,28 +24,22 @@ import java.util.List;
 public class SelectCategoryActivity extends AppCompatActivity {
     public static ArrayList<HolderClass> holderClasses;
     private ArrayList<HolderClass> selectedCategory;
+    public ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_category_activity);
 
+        this.progressBar = findViewById(R.id.category_loading);
+        progressBar.setVisibility(View.VISIBLE);
+
         selectedCategory = new ArrayList<>();
         holderClasses = new ArrayList<>();
-        holderClasses.add(new HolderClass("android" , R.drawable.ic_launcher_background));
-        holderClasses.add(new HolderClass("android" , R.drawable.ic_launcher_foreground));
-
-        GridView gridView = findViewById(R.id.category_select_view);
-        gridView.setAdapter(new CategoryAdapter(holderClasses));
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Drawable highlight = getResources().getDrawable(R.drawable.highlight);
-                view.findViewById(R.id.select_category_item_image).setBackground(highlight);
-                selectedCategory.add(holderClasses.get(i));
-            }
-        });
+        //holderClasses.add(new HolderClass("android" , R.drawable.ic_launcher_background));
+        //holderClasses.add(new HolderClass("android" , R.drawable.ic_launcher_foreground));
+        addCategories(holderClasses);
 
         Button cancel = (Button) findViewById(R.id.cancel_question);
         cancel.setOnClickListener(new View.OnClickListener() {
@@ -66,11 +62,29 @@ public class SelectCategoryActivity extends AppCompatActivity {
 
     }
 
+    private void addCategories(ArrayList<HolderClass> holderClasses) {
+        CategoryUtils.getAllCategories(holderClasses, this);
+    }
+
+    public void setUpHolders() {
+        GridView gridView = findViewById(R.id.category_select_view);
+        gridView.setAdapter(new CategoryAdapter(holderClasses));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Drawable highlight = getResources().getDrawable(R.drawable.highlight);
+                view.findViewById(R.id.select_category_item_image).setBackground(highlight);
+                selectedCategory.add(holderClasses.get(i));
+            }
+        });
+    }
+
     public static class HolderClass {
         public String name;
         public int image;
 
-        public HolderClass(String name, int image){
+        public HolderClass(String name, int image) {
             this.image = image;
             this.name = name;
         }
--- app/src/main/java/com/sharifdev/torobche/backUtils/CategoryUtils.java
+++ app/src/main/java/com/sharifdev/torobche/backUtils/CategoryUtils.java
@@ -1,7 +1,73 @@
 package com.sharifdev.torobche.backUtils;
 
+import android.app.Activity;
+import android.view.View;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.sharifdev.torobche.Category.SelectCategoryActivity;
+import com.sharifdev.torobche.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Work with categories table in Backend
  */
 public class CategoryUtils {
+    public static void getAllCategories(final ArrayList<SelectCategoryActivity.HolderClass> holderClasses,
+                                        final SelectCategoryActivity activity) {
+        final HashMap<String, Integer> categories = new HashMap<>();
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Category");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e != null)
+                    e.printStackTrace();
+                else {
+                    for (ParseObject parseObject : objects) {
+                        categories.put(((String) parseObject.get("name")), ((Integer) parseObject.get("icon_id")));
+                        // todo category image
+                        holderClasses.add(new SelectCategoryActivity.HolderClass(
+                                ((String) parseObject.get("name")),
+                                getCategoryImageByID(parseObject.getInt("icon_id"))
+                        ));
+                    }
+                    // do other things for holder
+                    activity.setUpHolders();
+                    // stop progress bar
+                    activity.progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+
+    private static int getCategoryImageByID(int id) {
+        switch (id) {
+            case 1:
+                return R.drawable.ic_technology;
+            case 2:
+                return R.drawable.ic_sport;
+            case 3:
+                return R.drawable.ic_games;
+            case 4:
+                return R.drawable.ic_history;
+            case 5:
+                return R.drawable.ic_food;
+            case 6:
+                return R.drawable.ic_education;
+            case 7:
+                return R.drawable.ic_business;
+            case 8:
+                return R.drawable.ic_tv;
+            case 9:
+                return R.drawable.ic_music;
+            default:
+                return R.drawable.ic_general;
+        }
+    }
 }
