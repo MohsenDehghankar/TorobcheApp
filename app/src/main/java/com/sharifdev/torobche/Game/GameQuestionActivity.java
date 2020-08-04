fb44ac5ac77598e8e76f92fa21c6c3b573d6a90f
z <z.y.j.1379@gmail.com>
2020-08-04 17:31:58 +0430

    edit GameQuestionActivity

--- app/src/main/java/com/sharifdev/torobche/Game/GameQuestionActivity.java
+++ app/src/main/java/com/sharifdev/torobche/Game/GameQuestionActivity.java
@@ -4,25 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.sharifdev.torobche.Home;
 import com.sharifdev.torobche.R;
 import com.sharifdev.torobche.model.Question;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class GameQuestionActivity extends AppCompatActivity {
     private int q_number = 0;
     private Button next;
-    Handler handelr = new Handler();
-    Runnable runnable;
-    ArrayList<Integer> answers = new ArrayList<>(10);
+    int[] answers = new int[10];
+    int h = 0;
+    CountDownTimer mCountDownTimer;
 
 
     @Override
@@ -39,8 +43,9 @@ public class GameQuestionActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mCountDownTimer.cancel();
                 if (q_number == 10) {
-                    //todo show correct answer
+                    //todo show correct answer( not important now)
                     saveAnswer();
                     Intent i = new Intent(getApplicationContext(), Home.class);
                     startActivity(i);
@@ -48,7 +53,7 @@ public class GameQuestionActivity extends AppCompatActivity {
                     finish();
                 }
                 if (q_number < 10) {
-                    //todo show correct answer
+                    //todo show correct answer( not important now)
                     saveAnswer();
                     //todo get random question
                     setQuestion(new Question());
@@ -63,22 +68,22 @@ public class GameQuestionActivity extends AppCompatActivity {
     private void saveAnswer(){
         ImageView imageView = (ImageView) findViewById(R.id.imageView1);
         if(imageView.getDrawable().getConstantState() ==getResources().getDrawable(R.drawable.correct).getConstantState()){
-            answers.set(q_number,1);
+            answers[q_number-1] = 1;
             return;
         }
         imageView = (ImageView) findViewById(R.id.imageView2);
         if(imageView.getDrawable().getConstantState() ==getResources().getDrawable(R.drawable.correct).getConstantState()){
-            answers.set(q_number,2);
+            answers[q_number-1] = 2;
             return;
         }
         imageView = (ImageView) findViewById(R.id.imageView3);
         if(imageView.getDrawable().getConstantState() ==getResources().getDrawable(R.drawable.correct).getConstantState()){
-            answers.set(q_number,3);
+            answers[q_number-1] = 3;
             return;
         }
         imageView = (ImageView) findViewById(R.id.imageView4);
         if(imageView.getDrawable().getConstantState() ==getResources().getDrawable(R.drawable.correct).getConstantState()){
-            answers.set(q_number,4);
+            answers[q_number-1] = 4;
         }
     }
 
@@ -97,7 +102,7 @@ public class GameQuestionActivity extends AppCompatActivity {
     }
 
     /**
-     * call it for each question (!!! set images no_photo if the question have not photo)
+     * call it for each question ( set images to no_photo if the question have not photo)
      */
     private void setQuestion(Question q) {
         fillComponent(R.id.q_image, q.questionImage, R.id.q_text, q.questionText);
@@ -105,21 +110,46 @@ public class GameQuestionActivity extends AppCompatActivity {
         fillComponent(R.id.c2_image, q.image2, R.id.c2_text, q.answerText2);
         fillComponent(R.id.c3_image, q.image3, R.id.c3_text, q.answerText3);
         fillComponent(R.id.c4_image, q.image4, R.id.c4_text, q.answerText4);
+        setDefaultImage((ImageView) findViewById(R.id.imageView1));
+        setDefaultImage((ImageView) findViewById(R.id.imageView2));
+        setDefaultImage((ImageView) findViewById(R.id.imageView3));
+        setDefaultImage((ImageView) findViewById(R.id.imageView4));
+
 
         q_number += 1;
 
         setTimeBar();
     }
 
+    private void setDefaultImage(ImageView image){
+        image.setImageResource(R.drawable.wrong);
+    }
     private void fillComponent(int imageId, int imageResource, int textId, String text) {
         ImageView image = findViewById(imageId);
         image.setImageResource(imageResource);
 
-        EditText q_text = findViewById(textId);
+        TextView q_text = findViewById(textId);
         q_text.setText(text);
     }
 
     private void setTimeBar() {
         final ProgressBar timeBar = findViewById(R.id.time_bar);
+        h = 0;
+        timeBar.setProgress(h);
+        mCountDownTimer=new CountDownTimer(10000,10) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                h++;
+                timeBar.setProgress((int)h*100/(10000/10));
+            }
+
+            @Override
+            public void onFinish() {
+                next.performClick();
+                timeBar.setProgress(100);
+            }
+        };
+        mCountDownTimer.start();
     }
 }
